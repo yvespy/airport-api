25ce46f8b8f2fd28d65b7c0c298b5c219706c7e0
yevpy <eugene2piskun@gmail.com>
2025-05-19 13:29:06 +0300

    Change settings to store information in .env

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-l6s9)h#9vww8aswenv8v1r2u_rhw9dxx*ojwj7i+b^42vov2$*"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split()
 
 
 # Application definition
